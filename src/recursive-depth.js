23ed40ca1a730d7854759738a52932dbaf4ea25a
MVRUN <55558413+MVRUN@users.noreply.github.com>
2021-03-23 19:01:50 +0300

    feat
    Optimized the code

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,17 +1,12 @@
 module.exports = class DepthCalculator {
   calculateDepth(arr, depth = 1) {
-    if(Array.isArray(arr) && arr.length === 0) {
-    return depth = 1;
-       }
-    else {
     arr.forEach(element => {
       if(Array.isArray(element)) {
         let counter = 1;
-        counter = counter + this.calculateDepth(element, counter);
+        counter+= this.calculateDepth(element, counter);
         depth = Math.max(depth, counter);
       }
     });
-    }
     return depth;
 };
 };
