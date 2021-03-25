fddab6ca914cc962d8ad6d3a5de0efcd08f3ce1f
MVRUN <55558413+MVRUN@users.noreply.github.com>
2021-03-25 16:21:37 +0300

    feat
    Add a solution to the task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,37 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let newOption = {
+    repeatTimes: 1,
+    separator: '+',
+    addition: '',
+    additionRepeatTimes: 1,
+    additionSeparator: '|'
+  }
+  
+  if (options.repeatTimes) {
+    newOption.repeatTimes = options.repeatTimes;
+  }
+  if (options.separator) {
+    options.separator = String(options.separator);
+    newOption.separator = options.separator;
+  }
+  if (options.additionRepeatTimes) {
+    newOption.additionRepeatTimes = options.additionRepeatTimes;
+  }
+  if (options.additionSeparator) {
+    options.additionSeparator = String(options.additionSeparator);
+    newOption.additionSeparator = options.additionSeparator;
+  }
+  if (typeof(str) != "string") {
+    str = String(str);
+  }
+  if(options.hasOwnProperty("addition")) {
+    newOption.addition = String(options.addition);
+  }
+  newOption.addition = newOption.addition + newOption.additionSeparator;
+  newOption.addition = newOption.addition.repeat(newOption.additionRepeatTimes).slice(0, - newOption.additionSeparator.length);
+  str = str + newOption.addition;
+  str = str + newOption.separator;
+  str = str.repeat(newOption.repeatTimes).slice(0, - newOption.separator.length);
+  return str;
 };
   
